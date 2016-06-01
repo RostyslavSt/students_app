ed11fb32ea375b22efc5a95a2899f0da9ca7ad3d
rost <rost@ukr.net>
2016-06-01 12:07:07 +0300

    qwt

--- templates/addCourse.js
+++
@@ -1,7 +0,0 @@
-var ADD_COURSE = `
-<div class='form-group'>
-	<label>Course 1:</label>
-	<input name='courses[]' class='form-control student-course'>
-	<a href='#' class='remove-course'>Remove course</a>
-</div>
- `
--- templates/studentRowView.js
+++ templates/studentRowView.js
@@ -25,11 +25,18 @@ var	NEW_COURSE = `
 </div>
 `
 
-var	NEW_COURSEE = `
-{{#courses}}
-	<div class="course-group">
-		<b>Course : </b>
-		<span class='student-course'>{{.}}</span>
-	</div>
-{{/courses}}
-`
+define(function() {
+    return {
+        rowView: `
+                                <tr>
+                                  <td>{{first_name}}</td>
+                                  <td>{{last_name}}</td>
+                                  <td data-id={{id}}>
+                                    <a href="#" class="btn btn-default">Show</a>
+                                    <a href="#" class="btn btn-primary">Edit</a>
+                                    <a href="#" class="btn btn-danger">Delete</a>
+                                  </td>
+                                </tr>
+                            `
+    };
+});
